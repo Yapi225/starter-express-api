7057cc40bfcbebb31f3e982c1cebed2b5cc08957
Yapi225 <55558062+Yapi225@users.noreply.github.com>
2023-06-01 19:28:19 +0000

    Update index.js : add cors

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { Pool } = require('pg');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(bodyParser.json());
 
 const pool = new Pool({
